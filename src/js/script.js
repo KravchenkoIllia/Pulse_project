b1440115d3cf08e2e5f728a0a1eb6fb9a275bb50
Illia <ddallmour@gmail.com>
2022-07-16 18:03:34 +0300

    tabs

--- src/js/script.js
+++ src/js/script.js
@@ -7,8 +7,27 @@ const slider = tns({
     controls: false
   });
 
-  document.querySelector('.next').addEventListener('click', function () {
-    slider.goTo('next');})
+document.querySelector('.next').addEventListener('click', function () {
+  slider.goTo('next');})
 
-  document.querySelector('.prev').addEventListener('click', function () {
-    slider.goTo('prev');}) 
+document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo('prev');}) 
+
+$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+  $(this)
+    .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+    .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+});
+
+function toggleSlide(item) {
+  $(item).each(function(i) {
+    $(this).on('click', function(e) {
+      e.preventDefault();
+      $('.catalog__item_content').eq(i).toggleClass('catalog__item_content_active');
+      $('.catalog__item_list').eq(i).toggleClass('catalog__item_list_active');
+    })
+  })
+};
+
+toggleSlide('.catalog__item_back');
+toggleSlide('.catalog__item_link');
